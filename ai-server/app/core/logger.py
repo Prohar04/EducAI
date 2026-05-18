1f798999d5fa013ccb2c63dc17c488719be2d832
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2026-05-18 11:56:20 +0600

    fast api vercel ready file
    when deploying into vercel, remember to add .env file

--- ai-server/app/core/logger.py
+++ ai-server/app/core/logger.py
@@ -3,9 +3,7 @@ import sys
 import os
 from logging.handlers import RotatingFileHandler
 
-log_dir = "logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+is_vercel = os.environ.get("VERCEL") == "1"
 
 logger = logging.getLogger("api_security")
 logger.setLevel(logging.INFO)
@@ -15,11 +13,15 @@ log_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messa
 stream_handler = logging.StreamHandler(sys.stdout)
 stream_handler.setFormatter(log_format)
 
-file_handler = RotatingFileHandler(os.path.join(log_dir, "api.log"), maxBytes=10 * 1024 * 1024, backupCount=5)
-file_handler.setFormatter(log_format)
-
 if not logger.handlers:
     logger.addHandler(stream_handler)
-    logger.addHandler(file_handler)
+    
+    if not is_vercel:
+        log_dir = "logs"
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        file_handler = RotatingFileHandler(os.path.join(log_dir, "api.log"), maxBytes=10 * 1024 * 1024, backupCount=5)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
 
 logger.propagate = False
